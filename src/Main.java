ad2907355d62a793911a5bd04628c9339d2ec6f1
mlriley77 <rileycode77@gmail.com>
2017-04-14 10:07:56 -0400

    update with loop

--- src/Main.java
+++ src/Main.java
@@ -5,24 +5,36 @@ public class Main {
 
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
+        String userAnswer;
 
         System.out.println("Welcome to the Casino");
 
-
-        System.out.println("How many sides should each die have: ");
-        int userInput = scan.nextInt();
+        do {
 
 
+            System.out.println("How many sides should each die have: ");
+            int userInput = scan.nextInt();
+            scan.nextLine();
 
-        System.out.println("Roll 1: ");
-        System.out.println(randomInt(userInput));
-        System.out.println(randomInt(userInput));
+
+            System.out.println("Roll 1: ");
+            System.out.println(randomInt(userInput));
+            System.out.println(randomInt(userInput));
+
+            System.out.println("Do you want to continue?(y/n) ");
+            userAnswer = scan.nextLine();
+
+
+        } while (userAnswer.equalsIgnoreCase("y"));
+
+
     }
-    public static int randomInt (int numDice) {
+
+
+    public static int randomInt(int numDice) {
         Random rand = new Random();
 
         return rand.nextInt(numDice) + 1;
-
 
 
     }
